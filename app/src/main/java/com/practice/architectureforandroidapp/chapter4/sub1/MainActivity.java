9e75bfb25f8b6de5b73ba2cc1f123480d304d0c8
dykim14 <dykim14@gmail.com>
2021-05-06 17:48:44 +0900

    Chapter 4의 1. 데이터 바인딩 #3

---
+++ app/src/main/java/com/practice/architectureforandroidapp/chapter4/sub1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.practice.architectureforandroidapp.chapter4.sub1;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.practice.architectureforandroidapp.R;
+import com.practice.architectureforandroidapp.databinding.ActivityMain2Binding;
+import com.practice.architectureforandroidapp.databinding.ActivityMainLayoutBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMain2Binding binding;
+    ActivityMainLayoutBinding binding2;
+
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main_2);
+        FrameLayout rootLayout = binding.container;
+        TextView textView = binding.tv;
+        textView.setText("Hello charles");
+        // Or
+        binding.setMyText("Hello charles");
+
+        binding2 = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_main_3, null, false);
+        binding2.setUser(new User("Charles", "Darwin"));
+    }
+}
--- app/src/test/java/com/practice/architectureforandroidapp/chapter4/Sub1Test.java
+++ app/src/test/java/com/practice/architectureforandroidapp/chapter4/Sub1Test.java
@@ -1,24 +1,19 @@
 package com.practice.architectureforandroidapp.chapter4;
 
-import android.view.LayoutInflater;
-import androidx.test.*;
-
-import com.practice.architectureforandroidapp.databinding.UserBinding;
-
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
 public class Sub1Test {
-
-    @Rule
-    ActivityTestRule 
-
 
     @Test
     public void testBindingClassVariable() {
-        ActivityTestRule
-        UserBinding binding = LayoutInflater.from()
+//        ActivityMain2Binding binding = ActivityMain2Binding.inflate(layoutInflater);
+//        binding = ActivityMain2Binding.bind(rootView);
+//        binding = DataBindingUtil.inflate(
+//                layoutInflater,
+//                R.layout.activity_main_2,
+//                parent,
+//                attachToParent
+//        );
+//        binding = DataBindingUtil.bind(rootView);
     }
 }
